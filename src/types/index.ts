3c6c40b5f843304bc4fb7c058f6403f2671a7f44
Humed-Muhammad <abc45171@gmail.com>
2023-03-24 14:22:44 +0300

    ✏️ Types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,17 @@
+import { DocumentData } from "firebase/firestore";
+
 export interface IAddWeeData {
   weeMeasurement: string;
   weeML: undefined;
-  weeTime: string;
+  weeTime: Date;
   creatingDoc: boolean;
   urgency: boolean;
   incontinence: boolean;
 }
 
 export type ILastSevenDaysData = Omit<IAddWeeData, "creatingDoc">;
+
+export interface IHomeState {
+  lastSevenDays: ILastSevenDaysData[] | undefined | DocumentData;
+  sevenDaysAverage: number | undefined;
+}
