85aa2a93bb4dd932cc0c2ceeaf78bf19d26d1ddd
Humed-Muhammad <abc45171@gmail.com>
2023-03-27 23:21:50 +0300

    💄 UI: BarChart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,40 @@
+<template>
+    <Bar id="my-chart-id" :options="{
+        responsive: true,
+    }" :data="
+    {
+        labels: storeState?.chartLabel,
+        datasets: [{
+            label: 'Wees During the Day',
+            data: storeState?.chartData,
+            backgroundColor: [
+                'rgba(255, 206, 96, 0.4)',
+            ],
+            borderColor: [
+                'rgba(255, 206, 96, 1)',
+            ],
+            borderWidth: 1
+        }],
+
+    }" />
+</template>
+  
+<script lang="ts">
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { storeState } from "@/store"
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+    name: 'BarChart',
+    components: { Bar },
+    setup() {
+
+        return {
+            storeState,
+
+        }
+    },
+
+}
+</script>
