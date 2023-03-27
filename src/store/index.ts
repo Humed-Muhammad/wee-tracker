2f64b7baa20c05297d62126d9390065f00ffc533
Humed-Muhammad <abc45171@gmail.com>
2023-03-27 23:22:25 +0300

    ✨ NEW: Global states

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { IUser } from "@/types";
-import { getUSerData } from "@/utils/helpers";
+import { getUSerData, getWeesByDay, updateUserData } from "@/utils/helpers";
+import { addDays, subDays } from "date-fns";
 import { reactive } from "vue";
 
 export const storeState = reactive<IUser>({
@@ -10,7 +11,28 @@ export const storeState = reactive<IUser>({
     uid: "",
     weeMeasurement: "",
   },
+  updatingData: false,
   fetchUserData() {
     getUSerData(storeState);
+  },
+  updateUserWeeMeasurement() {
+    updateUserData(storeState);
+  },
+  weesDuringDay: [],
+  chartLabel: [],
+  chartData: [],
+  averageWeeDuringDay: undefined,
+  currentDate: new Date(),
+  fetchingWees: false,
+  requestWeesByDay() {
+    getWeesByDay(storeState);
+  },
+  handleAddDays() {
+    storeState.currentDate = addDays(storeState.currentDate, 1);
+    storeState.requestWeesByDay();
+  },
+  handleSubDays() {
+    storeState.currentDate = subDays(storeState.currentDate, 1);
+    storeState.requestWeesByDay();
   },
 });
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,10 @@ import { DocumentData } from "firebase/firestore";
 export interface IAddWeeData {
   weeMeasurement: string;
   weeML: undefined;
-  weeTime: Date;
+  weeTime: {
+    date: Date;
+    time: string;
+  };
   creatingDoc: boolean;
   urgency: boolean;
   incontinence: boolean;
@@ -13,9 +16,14 @@ export type IWeeDuringDaysData = Omit<IAddWeeData, "creatingDoc">;
 
 export interface IHomeState {
   weesDuringDay: IWeeDuringDaysData[] | undefined | DocumentData;
+  chartLabel: string[];
+  chartData: number[];
   averageWeeDuringDay: number | undefined;
   currentDate: Date;
   fetchingWees: boolean;
+  requestWeesByDay: () => void;
+  handleAddDays: () => void;
+  handleSubDays: () => void;
 }
 
 export interface IUserInfo {
@@ -27,7 +35,9 @@ export interface IUserInfo {
   uid: string;
 }
 
-export interface IUser {
+export interface IUser extends IHomeState {
   user: IUserInfo | DocumentData;
+  updatingData: boolean;
   fetchUserData: () => void;
+  updateUserWeeMeasurement: () => void;
 }
