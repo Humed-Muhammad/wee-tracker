9203d5bb6ae9300f3c779b812ec8368795947f3f
Humed-Muhammad <abc45171@gmail.com>
2023-04-01 00:03:13 +0300

    ✨ SignIn with google shared component

---
+++ src/components/shared/SigninWithGoogle.vue
@@ -0,0 +1,30 @@
+<template>
+    <ion-button v-if="!props.isSigningIn" color="light" class="ion-margin signInBtn" @click="props.command">
+        <img width="35" src="assets/google.png" />
+        {{ text || " Sign In With Google" }}
+    </ion-button>
+    <ion-button color="dark" class="ion-margin signInBtn" v-else>
+        <ion-spinner name="dots" />
+    </ion-button>
+</template>
+
+<script setup lang="ts" >
+import { defineProps } from 'vue';
+import {
+    IonButton,
+    IonSpinner
+} from "@ionic/vue"
+const props = defineProps<{
+    command: () => void,
+    isSigningIn: boolean,
+    text?: string,
+}>()
+</script>
+
+<style scoped>
+.signInBtn {
+    width: 90%;
+    font-size: 16px;
+    text-align: center;
+}
+</style>
