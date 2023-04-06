4857fe2c8b6836adf336fd08801520589b90e282
Humed-Muhammad <abc45171@gmail.com>
2023-04-06 22:13:13 +0300

    ✨ New Chart To pdf

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
+import { toPng } from "html-to-image";
+import jsPDF from "jspdf";
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
@@ -16,3 +18,51 @@ const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 export const provider = new GoogleAuthProvider();
 export const auth = getAuth();
+
+export async function exportMultipleChartsToPdf() {
+  console.log("first");
+  const doc = new jsPDF("p", "px"); // (1)
+
+  const elements = document.getElementsByClassName("chart-container"); // (2)
+  console.log(elements);
+  await creatPdf({ doc, elements }); // (3-5)
+
+  doc.save(`charts.pdf`); // (6)
+}
+
+async function creatPdf({
+  doc,
+  elements,
+}: {
+  doc: jsPDF;
+  elements: HTMLCollectionOf<Element>;
+}) {
+  let top = 20;
+  const padding = 10;
+
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements.item(i) as HTMLElement;
+    const imgData = await toPng(el);
+
+    let elHeight = el.offsetHeight;
+    let elWidth = el.offsetWidth;
+
+    const pageWidth = doc.internal.pageSize.getWidth();
+
+    if (elWidth > pageWidth) {
+      const ratio = pageWidth / elWidth;
+      elHeight = elHeight * ratio - padding;
+      elWidth = elWidth * ratio - padding;
+    }
+
+    const pageHeight = doc.internal.pageSize.getHeight();
+
+    if (top + elHeight > pageHeight) {
+      doc.addPage();
+      top = 20;
+    }
+
+    doc.addImage(imgData, "PNG", padding, top, elWidth, elHeight, `image${i}`);
+    top += elHeight;
+  }
+}
